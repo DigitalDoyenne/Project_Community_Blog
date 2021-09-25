8dfde5106bb31d1dd45c28ab9dd75da1274b8788
digitaldoyenne <digitaldoyenne@protonmail.com>
2021-09-24 22:43:31 -0500

    Init commit

---
+++ build/precache-manifest.6602e4f325e11c34a91d11850106a484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78291c93bddffa1a2bb4ca5191ecddc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0cd2d74f8905777b570",
+    "url": "/static/css/main.922aec08.chunk.css"
+  },
+  {
+    "revision": "707ecc5b929afbdddc94",
+    "url": "/static/js/2.51cffeb9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.51cffeb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0cd2d74f8905777b570",
+    "url": "/static/js/main.cce92f81.chunk.js"
+  },
+  {
+    "revision": "66258a5d898a5a489f4f",
+    "url": "/static/js/runtime-main.1a96dc8f.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Fragment } from "react";
+
+import Header from "./common/Header";
+import NotFound from "./common/NotFound";
+import CardList from "./home/CardList";
+import User from "./user/User";
+import { Switch, Route } from "react-router-dom";
+
+function App() {
+  /*
+    TODO: There is no need to add a <Router >, it is in index.js.
+
+    The <CardList /> component should be shown when the user is on the index path.
+
+    The <User /> component should be shown when the user is on the following path:
+    /users/:userId
+
+    Display <NotFound /> when appropriate
+  */
+
+  return (
+    <Fragment>
+      <Header />
+
+      <Switch>
+        <Route path={"/users/:userId"}>
+          <User />
+        </Route>
+
+        <Route exact path={"/"}>
+          <CardList />
+        </Route>
+
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Fragment>
+  );
+}
+
+export default App;
